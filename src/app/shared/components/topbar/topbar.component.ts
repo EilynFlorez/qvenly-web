fd1ef8fdc44961208e330352e303bafae25be363
LauraAlejandraG12 <alejandrag900800qgmail.com>
2026-05-26 22:42:54 -0500

    feat(shared/components): create nav-admin sidebar layout and topbar components

---
+++ src/app/shared/components/topbar/topbar.component.ts
@@ -0,0 +1,12 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-topbar',
+  templateUrl: './topbar.component.html',
+  styleUrl: './topbar.component.scss'
+})
+export class TopbarComponent {
+      @Input() title: string = '';
+      @Input() subtitle: string = '';
+      @Input() showExport: boolean = false;
+}
