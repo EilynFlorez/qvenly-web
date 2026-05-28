fdd3687ff05f09f174e7767bd69d066b0f4b07b1
LauraAlejandraG12 <alejandrag900800qgmail.com>
2026-05-27 22:47:11 -0500

    feat(shared): add SkeletonComponent to shared module

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,20 +2,23 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TopbarComponent } from './components/topbar/topbar.component';
 import { NavAdminComponent } from './components/nav-admin/nav-admin.component';
+import { SkeletonComponent } from './components/skeleton/skeleton.component';
 
 
 
 @NgModule({
   declarations: [
     TopbarComponent,
-    NavAdminComponent
+    NavAdminComponent,
+    SkeletonComponent
   ],
   imports: [
     CommonModule
   ],
   exports: [
     NavAdminComponent,
-    TopbarComponent
+    TopbarComponent, 
+    SkeletonComponent,
   ]
 })
 export class SharedModule { }
