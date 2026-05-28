1bbac8fa1b6b5a6aea8d771e87a7c9999efe7f5f
LauraAlejandraG12 <alejandrag900800qgmail.com>
2026-05-27 22:15:40 -0500

    feat(dashboard): configure dashboard module and routing

--- src/app/features/dashboard/dashboard.module.ts
+++ src/app/features/dashboard/dashboard.module.ts
@@ -2,13 +2,32 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { DashboardRoutingModule } from './dashboard-routing.module';
+import { DashboardComponent } from './dashboard.component';
+import { KpiCardsComponent } from './pages/kpi-cards/kpi-cards.component';
+import { PlanChartComponent } from './pages/plan-chart/plan-chart.component';
+import { MonthlyChartComponent } from './pages/monthly-chart/monthly-chart.component';
+import { OrganizerChartComponent } from './pages/organizer-chart/organizer-chart.component';
+import { UsersByEventComponent } from './pages/users-by-event/users-by-event.component';
+import { SharedModule } from '../../shared/shared.module';
+import { FiltersComponent } from './pages/filters/filters.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    DashboardComponent,
+    KpiCardsComponent,
+    PlanChartComponent,
+    MonthlyChartComponent,
+    OrganizerChartComponent,
+    UsersByEventComponent,
+    FiltersComponent, 
+  ],
   imports: [
     CommonModule,
-    DashboardRoutingModule
+    DashboardRoutingModule,
+    SharedModule,
+    ReactiveFormsModule,
   ]
 })
 export class DashboardModule { }
