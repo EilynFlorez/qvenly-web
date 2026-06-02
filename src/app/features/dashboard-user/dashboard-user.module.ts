5ee8791ccebfe9f4c152257567980cd12599e021
LeidyMaingualDev <vanesamaingual2008@gmail.com>
2026-06-02 17:59:09 -0500

    feat(dashboard-user): build user dashboard with active plan, available plans and quick actions

--- src/app/features/dashboard-user/dashboard-user.module.ts
+++ src/app/features/dashboard-user/dashboard-user.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 import { DashboardUserRoutingModule } from './dashboard-user-routing.module';
 import { DashboardUserComponent } from './pages/dashboard-user/dashboard-user.component';
-
 
 @NgModule({
   declarations: [
@@ -11,6 +11,7 @@ import { DashboardUserComponent } from './pages/dashboard-user/dashboard-user.co
   ],
   imports: [
     CommonModule,
+    RouterModule,
     DashboardUserRoutingModule
   ]
 })
